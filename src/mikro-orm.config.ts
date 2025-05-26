ff8e46b29e49d4accbb2315d34242e641f998ada
Sylvester Kwon <sylvesterkwon@gmail.com>
2025-05-26 19:03:19 +0900

    feat: Add docker-compose.yaml & dockerfile

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -9,13 +9,13 @@ register();
 
 // https://mikro-orm.io/docs/next/configuration#using-environment-variables
 const config: Options = {
-  host: process.env.MIKRO_ORM_HOST,
-  port: process.env.MIKRO_ORM_PORT
-    ? parseInt(process.env.MIKRO_ORM_PORT, 10)
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT
+    ? parseInt(process.env.POSTGRES_PORT, 10)
     : undefined,
-  user: process.env.MIKRO_ORM_USER,
-  password: process.env.MIKRO_ORM_PASSWORD,
-  dbName: process.env.MIKRO_ORM_DB_NAME,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  dbName: process.env.POSTGRES_DB_NAME,
   debug: process.env.MIKRO_ORM_DEBUG === 'true',
   driver: PostgreSqlDriver,
   entities: ['dist/**/*.entity.js'],
