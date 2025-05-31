85d58b60265c8aa114fb4e6dd9c986b674c4a12d
Sylvester Kwon <sylvesterkwon@gmail.com>
2025-05-31 21:35:26 +0900

    fix: Fixed CommandModule dependency

--- src/commands/command.module.ts
+++ src/commands/command.module.ts
@@ -3,10 +3,30 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { CodeforcesSyncCommand } from './codeforces-sync.command';
 import { ProblemModule } from 'src/problem/problem.module';
 import { AtCoderSyncCommand } from './atcoder-sync.command';
+import { EventManagerModule } from 'src/event-manager/event-manager.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { ClsModule } from 'nestjs-cls';
+import { ConfigModule } from '@nestjs/config';
+import authConfig from 'src/config/auth.config';
 
 @Module({
-  imports: [MikroOrmModule.forRoot(), ProblemModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env`],
+      load: [authConfig],
+      isGlobal: true,
+    }),
+    MikroOrmModule.forRoot(),
+    EventManagerModule,
+    EventEmitterModule.forRoot(),
+    ClsModule.forRoot({
+      middleware: {
+        mount: true,
+      },
+      global: true,
+    }),
+    ProblemModule,
+  ],
   providers: [CodeforcesSyncCommand, AtCoderSyncCommand],
-  controllers: [],
 })
 export class CommandModule {}
